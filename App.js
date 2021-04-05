9a45f80536b00487d3fd87a7074cc1d696445b3f
abuhumayed <ahmedthanjim@hotmail.co.uk>
2021-04-05 14:58:11 +0100

    Routes Nav Update

--- App.js
+++ App.js
@@ -1,45 +1,9 @@
 import 'react-native-gesture-handler';
-import React,{useEffect} from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import OnboardingScreen from './screens/OnboardingScreen';
-import LoginScreen from './screens/LoginScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const AppStack = createStackNavigator();
+import React from 'react'
+import Providers from './navigation'
 
 const App = () => {
-const [isFirstLaunch,setIsFirstLaunch] = React.useState(null);
-
-useEffect(() => {
-  AsyncStorage.getItem('alreadyLaunched').then(value => {
-  if (value === null){
-    AsyncStorage.setItem('alreadyLaunched','true');
-    setIsFirstLaunch(true);
-  }else{
-    setIsFirstLaunch(false);
-  }
-  });
-}, []);
-
-
-if (isFirstLaunch === null){
-  return null;
-}else if (isFirstLaunch === true ){
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator
-      headerMode = 'none'
-      >
-    <AppStack.Screen name = "Onboarding" component ={OnboardingScreen} />
-     <AppStack.Screen name = "Login" component ={LoginScreen} />
-      </AppStack.Navigator>
-   </NavigationContainer>
-  );
-} else {
-    return <LoginScreen/>
- 
-  }
+    return <Providers/>
 }
 
-export default App;
+export default App
---
+++ navigation/Routes.js
@@ -0,0 +1,16 @@
+
+import React, {useState,useEffect,useContext} from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+import AuthStack from './AuthStack';
+
+
+const Routes = () => {
+  
+  return (
+    <NavigationContainer>
+      <AuthStack/>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
