636008d6592eaa8b26247efdc3b5eb7c735664ca
abuhumayed <ahmedthanjim@hotmail.co.uk>
2021-04-05 12:27:48 +0100

    Update

---
+++ screens/LoginScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+
+const LoginScreen = () => {
+    return (
+        <View style = {styles.container} >
+            <Text>Login</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent : 'center'
+    }
+       
+})
